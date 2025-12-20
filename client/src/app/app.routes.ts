c22d3379c5a631241c1495a8e2062294191340e4
asif elahi <asifelahi488@students.diu.ac>
2025-12-21 04:34:39 +0600

    wire and setup tailwind css with angular

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './features/auth/pages/login-page/login-page';
+import { SignupPage } from './features/auth/pages/signup-page/signup-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./features/auth/pages/signup-page/signup-page').then((m) => m.SignupPage),
+  },
+
+  //   {
+  //     path: 'signup',
+  //     component: SignupPage,
+  //   },
+];
---
+++ client/src/app/features/students/pages/student-details-page/student-details-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-details-page',
+  imports: [],
+  templateUrl: './student-details-page.html',
+  styleUrl: './student-details-page.scss',
+})
+export class StudentDetailsPage {
+
+}
