c1073539bf781240730080625d945b5d949cf156
asif elahi <asifelahi488@students.diu.ac>
2025-12-23 06:13:15 +0600

    adjust app.routes.ts for login routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,38 +5,6 @@ export const routes: Routes = [
   {
     path: '',
     component: AppShellLayout,
-    children: [
-      // ✅ Redirect only for the empty URL
-      { path: '', pathMatch: 'full', redirectTo: 'students' },
-
-      // /login
-      {
-        path: 'login',
-        loadComponent: () =>
-          import('./features/auth/pages/login-page/login-page').then((m) => m.LoginPage),
-      },
-
-      // ✅ Put the more specific route first (good practice)
-      // /students/:id
-      {
-        path: 'students/:id',
-        loadComponent: () =>
-          import('./features/students/pages/student-details-page/student-details-page').then(
-            (m) => m.StudentDetailsPage
-          ),
-      },
-
-      // /students
-      {
-        path: 'students',
-        loadComponent: () =>
-          import('./features/students/pages/students-list-page/students-list-page').then(
-            (m) => m.StudentsListPage
-          ),
-      },
-
-      // ✅ Wildcard last
-      { path: '**', redirectTo: 'students' },
-    ],
+    loadChildren: () => import('./shared/app-shell/app-shell.routes').then((m) => m.appShellRoutes),
   },
 ];
