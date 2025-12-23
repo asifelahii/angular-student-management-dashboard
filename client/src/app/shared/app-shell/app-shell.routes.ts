3cba2a64e40636519c5a0ba161f0d32d577ac547
asif elahi <asifelahi488@students.diu.ac>
2025-12-23 06:16:27 +0600

    add shared app-shell component

---
+++ client/src/app/shared/app-shell/app-shell.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+
+export const appShellRoutes: Routes = [
+    
+  // ✅ Redirect only for the empty URL
+  { path: '', pathMatch: 'full', redirectTo: 'students' },
+
+  // /login
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('../../features/auth/pages/login-page/login-page').then((m) => m.LoginPage),
+  },
+
+  // /students/:id
+  {
+    path: 'students/:id',
+    loadComponent: () =>
+      import('../../features/students/pages/student-details-page/student-details-page').then(
+        (m) => m.StudentDetailsPage
+      ),
+  },
+
+  // /students
+  {
+    path: 'students',
+    loadComponent: () =>
+      import('../../features/students/pages/students-list-page/students-list-page').then(
+        (m) => m.StudentsListPage
+      ),
+  },
+
+  // ✅ Wildcard last
+  { path: '**', redirectTo: 'students' },
+];
